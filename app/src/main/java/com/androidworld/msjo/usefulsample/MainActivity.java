d17ca955c09cfb6a9018672bfa35a42c0c57498c
MyoungsuJo <lezo22@gmail.com>
2017-04-24 02:49:09 +0900

    version 2

--- app/src/main/java/com/androidworld/msjo/usefulsample/MainActivity.java
+++ app/src/main/java/com/androidworld/msjo/usefulsample/MainActivity.java
@@ -1,13 +1,40 @@
 package com.androidworld.msjo.usefulsample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.tvOne)
+    TextView tvOne;
+    @BindView(R.id.btnOne)
+    Button btnOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+    @OnClick(R.id.btnOne)
+    void onClick() {
+        Toast.makeText(this, "onClick", Toast.LENGTH_SHORT).show();
     }
 }
