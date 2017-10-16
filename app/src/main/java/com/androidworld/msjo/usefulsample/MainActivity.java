541df94b990640476a42994e54f4f1bf553104ba
MyoungsuJo <lezo22@gmail.com>
2017-10-16 16:48:53 +0900

    test ....

--- app/src/main/java/com/androidworld/msjo/usefulsample/MainActivity.java
+++ app/src/main/java/com/androidworld/msjo/usefulsample/MainActivity.java
@@ -1,55 +1,175 @@
 package com.androidworld.msjo.usefulsample;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.squareup.picasso.Picasso;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.LinearLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity{
 
 
-    @BindView(R.id.tvOne)
-    TextView tvOne;
-    @BindView(R.id.btnOne)
-    Button btnOne;
-    @BindView(R.id.btnTwo)
-    Button btnTwo;
+    int nImgCount = 0;
 
-    @BindView(R.id.imgOne)
-    ImageView imgOne;
+    @BindView(R.id.view)
+    View view;
 
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        PermissionSample.testPermission(this);
+        setContentView(R.layout.cameratest);
 
         ButterKnife.bind(this);
 
-        Picasso.with(this).load("https://www.google.co.kr/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png").into(imgOne);
+//        Cursor cursor = loadCursor();
+//        nImgCount = cursor.getCount();
+//        Log.d("jms","jms imgcount : " + nImgCount);
+//
+//        Intent i = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+//        startActivityForResult(i,3344);
 
-
-        CircularFloatingActionMenu.showCircularFloatingActionMenu(this);
+        expand(view);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+//    /**
+//     * Called when an activity you launched exits, giving you the requestCode
+//     * you started it with, the resultCode it returned, and any additional
+//     * data from it.  The <var>resultCode</var> will be
+//     * {@link #RESULT_CANCELED} if the activity explicitly returned that,
+//     * didn't return any result, or crashed during its operation.
+//     * <p>
+//     * <p>You will receive this call immediately before onResume() when your
+//     * activity is re-starting.
+//     * <p>
+//     * <p>This method is never invoked if your activity sets
+//     * {@link android.R.styleable#AndroidManifestActivity_noHistory noHistory} to
+//     * <code>true</code>.
+//     *
+//     * @param requestCode The integer request code originally supplied to
+//     *                    startActivityForResult(), allowing you to identify who this
+//     *                    result came from.
+//     * @param resultCode  The integer result code returned by the child activity
+//     *                    through its setResult().
+//     * @param data        An Intent, which can return result data to the caller
+//     *                    (various data can be attached to Intent "extras").
+//     * @see #startActivityForResult
+//     * @see #createPendingResult
+//     * @see #setResult(int)
+//     */
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        Log.i("jms","onActivityResult : " + requestCode);
+////        super.onActivityResult(requestCode, resultCode, data);
+//        exitingCamera();
+//    }
+//
+//    public Cursor loadCursor() {
+//
+//        final String[] columns = { MediaStore.Images.Media.DATA,
+//                MediaStore.Images.Media._ID };
+//
+//        final String orderBy = MediaStore.Images.Media.DATE_ADDED;
+//
+//        return getContentResolver().query(
+//                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, columns, null,
+//                null, orderBy);
+//    }
+//
+//    public String[] getImagePaths(Cursor cursor, int startPosition) {
+//
+//        int size = cursor.getCount() - startPosition;
+//
+//        if (size <= 0)
+//            return null;
+//
+//        String[] paths = new String[size];
+//
+//        int dataColumnIndex = cursor
+//                .getColumnIndex(MediaStore.Images.Media.DATA);
+//
+//        for (int i = startPosition; i < cursor.getCount(); i++) {
+//
+//            cursor.moveToPosition(i);
+//
+//            paths[i - startPosition] = cursor.getString(dataColumnIndex);
+//        }
+//
+//        return paths;
+//    }
+//
+//    private void exitingCamera() {
+//
+//        Cursor cursor = loadCursor();
+//        String[] paths = getImagePaths(cursor, nImgCount);
+//
+//        Log.d("jms","jms length : " + paths.length);
+//        Log.d("jms","jms path : " + paths[0]);
+//
+//        // 이미지 프로세싱 작업
+////        new ProcessImage(paths).execute();
+//
+//        cursor.close();
+//
+//
+//    }
+
+
+    public static void expand(final View v) {
+        v.measure(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final int targetHeight = v.getMeasuredHeight();
+
+        // Older versions of android (pre API 21) cancel animations for views with a height of 0.
+        v.getLayoutParams().height = 1;
+        v.setVisibility(View.VISIBLE);
+        Animation a = new Animation()
+        {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                v.getLayoutParams().height = interpolatedTime == 1
+                        ? LinearLayout.LayoutParams.WRAP_CONTENT
+                        : (int)(targetHeight * interpolatedTime);
+                v.requestLayout();
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int)(targetHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
     }
 
+    public static void collapse(final View v) {
+        final int initialHeight = v.getMeasuredHeight();
 
-    @OnClick({R.id.btnOne, R.id.btnTwo})
-    void onClick() {
-        Toast.makeText(this, "onClick", Toast.LENGTH_SHORT).show();
+        Animation a = new Animation()
+        {
+            @Override
+            protected void applyTransformation(float interpolatedTime, Transformation t) {
+                if(interpolatedTime == 1){
+                    v.setVisibility(View.GONE);
+                }else{
+                    v.getLayoutParams().height = initialHeight - (int)(initialHeight * interpolatedTime);
+                    v.requestLayout();
+                }
+            }
+
+            @Override
+            public boolean willChangeBounds() {
+                return true;
+            }
+        };
+
+        // 1dp/ms
+        a.setDuration((int)(initialHeight / v.getContext().getResources().getDisplayMetrics().density));
+        v.startAnimation(a);
     }
 }
